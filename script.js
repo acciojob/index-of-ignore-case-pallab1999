4a7e73fc62678df9537be74508205dcf264aed0e
Pallab dutta <121430112+pallab1999@users.noreply.github.com>
2025-03-13 11:24:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	for(int i=0;i<s1.length;i++){
+		string s = s1.substring(i);
+		if(s.length>=s2.length){
+			if(s.substring(i,i+s2.length-1)===s2.substr(0)){
+				return i;
+			}
+		}
+	}
+	return -1;
 }
 
 // Please do not change the code below
